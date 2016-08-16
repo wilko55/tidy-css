3949d88a5821cc973e822e8893e596665fd01931
Andrew Wilkinson <andyjwilko@gmail.com>
2016-08-16 14:56:20 +0100

    First commit - eg site with basic features

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', (process.env.PORT || 7000));
+
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('express-partial-templates')(app));
+app.engine('html', require('hogan-express-strict'));
+
+app.get('/', function(req,res, next) {
+  res.render('index')
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
